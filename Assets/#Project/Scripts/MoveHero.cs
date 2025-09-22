2ef71022737b33339ae1453ab9bedd300eec21fa
Thomas Stassin <thomas.stassin@interface3.be>
2025-09-22 12:19:43 +0200

    jump

--- Assets/#Project/Scripts/MoveHero.cs
+++ Assets/#Project/Scripts/MoveHero.cs
@@ -5,15 +5,20 @@ using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(SpriteRenderer))]
 [RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class MoveHero : MonoBehaviour
 {
 
     [SerializeField] private InputActionAsset actions;
     [SerializeField] private float speed;
+    [SerializeField] private float jumpForce = 300f;
     private InputAction xAxis;
 
     private SpriteRenderer renderer;
     private Animator animator;
+
+    private Rigidbody2D rb;
+    private bool isJumping = false;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -22,22 +27,35 @@ public class MoveHero : MonoBehaviour
         xAxis = actions.FindActionMap("Luigi").FindAction("XAxis");
         renderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void OnEnable()
     {
         actions.FindActionMap("Luigi").Enable();
+        actions.FindActionMap("Luigi").FindAction("Jump").performed += OnJump;
     }
 
     private void OnDisable()
     {
         actions.FindActionMap("Luigi").Disable();
+        actions.FindActionMap("Luigi").FindAction("Jump").performed -= OnJump;
     }
 
     // Update is called once per frame
     void Update()
     {
         MoveX();
+
+        if(isJumping){
+            {
+                
+            }
+            if(rb.linearVelocityY < 0f) {
+                isJumping = false;
+                animator.SetBool("on jump", false);
+            }
+        }
     }
 
     private void MoveX()
@@ -46,5 +64,11 @@ public class MoveHero : MonoBehaviour
         renderer.flipX = xAxis.ReadValue<float>() < 0;
         animator.SetFloat("speed", Mathf.Abs(xAxis.ReadValue<float>()));
         transform.Translate(xAxis.ReadValue<float>() * speed * Time.deltaTime, 0f, 0f);
+    }
+
+    private void OnJump(InputAction .CallbackContext context){
+        rb.AddForce(Vector3.up * jumpForce);
+        animator.SetBool("on jump", true);
+        isJumping = true;
     }
 }
